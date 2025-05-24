2f423865bc91ba00237c622743aa35a3865bacf4
Leonardo Oliveira <40525909+haisesz@users.noreply.github.com>
2025-05-24 13:59:04 -0300

    Delete Tela de Login directory

--- Tela de Login/Conexao.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Tela_de_Login
-{
-    public static class Conexao
-    {
-        public static string ConexaoString { get; } =
-            "Host=localhost;Port=5432;Database=pim;User ID=postgres;Password=belofode";
-    }
-}
